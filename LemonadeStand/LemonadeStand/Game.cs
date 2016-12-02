65bbc2cd1aadc90bab47b02cd7cd5d48de2c5a68
Grant Smith <smithge87@gmail.com>
2016-12-01 20:20:44 -0600

    added first classes and welcome screen

---
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Game
+    {
+         public void StartGame()
+        {
+            EnterGame();
+        }
+        public void EnterGame()
+        {
+            Console.WriteLine("\n\n\n\n\n\n\nWELCOME TO LEMONADE STAND!");
+            Console.WriteLine("==========================\n");
+            Console.WriteLine("Please press'enter' to begin");
+            Console.ReadLine();
+        }
+    }
+}
